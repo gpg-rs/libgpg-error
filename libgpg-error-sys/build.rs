ddf6dbf7ebd607f8f9ec03249ac179b23bc133ba
John Schug <john.ips.schug@gmail.com>
2023-12-28 18:54:07 -0800

    Change windows install dir detection

--- libgpg-error-sys/build.rs
+++ libgpg-error-sys/build.rs
@@ -1,17 +1,13 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    if cfg!(windows) && try_registry() {
+    #[cfg(windows)]
+    if try_registry() {
         return Ok(());
     }
 
     system_deps::Config::new().probe()?;
     Ok(())
-}
-
-#[cfg(not(windows))]
-fn try_registry() -> bool {
-    false
 }
 
 #[cfg(windows)]
@@ -26,35 +22,19 @@ fn try_registry() -> bool {
     }
 
     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-    let key = match hklm.open_subkey("SOFTWARE\\GnuPG") {
-        Ok(k) => k,
-        Err(_) => {
-            // check if we are on 64bit windows but have 32bit GnuPG installed
-            match hklm.open_subkey("SOFTWARE\\WOW6432Node\\GnuPG") {
-                Ok(k) => {
-                    // found 32bit library
-                    if build::cargo_cfg_pointer_width() == 32 {
-                        eprintln!("compile using i586/686 target.");
-                        return false;
-                    } else {
-                        k
-                    }
-                }
-                Err(_) => {
-                    eprintln!("unable to retrieve install location");
-                    return false;
-                }
-            }
-        }
+    let Ok(key) = hklm.open_subkey_with_flags(r"SOFTWARE\GnuPG", KEY_WOW64_32KEY) else {
+        eprintln!("unable to retrieve install location");
+        return false;
     };
-    let root = match key.get_value::<String, _>("Install Directory") {
-        Ok(v) => PathBuf::from(v),
-        Err(_) => {
-            eprintln!("unable to retrieve install location");
-            return false;
-        }
+    let Ok(root) = key
+        .get_value::<OsString, _>("Install Directory")
+        .map(PathBuf::from)
+    else {
+        eprintln!("unable to retrieve install location");
+        return false;
     };
     println!("detected install via registry: {}", root.display());
+
     if root.join("lib/libgpg-error.imp").exists() {
         if let Err(e) = fs::copy(
             root.join("lib/libgpg-error.imp"),
