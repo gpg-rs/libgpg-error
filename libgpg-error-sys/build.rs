d38b017f4662fe3d91a92c095bb326972638235c
John Schug <john.ips.schug@gmail.com>
2024-11-18 17:49:05 -0800

    Add support for 64-bit windows builds.

--- libgpg-error-sys/build.rs
+++ libgpg-error-sys/build.rs
@@ -1,17 +1,19 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    if build::cargo_cfg_windows() && build::cargo_feature("windows_raw_dylib") {
-        return Ok(()); // neccessary linker args set in lib.rs
-    }
+    build::rerun_if_changed("build.rs");
 
-    #[cfg(windows)]
-    if try_registry() {
+    if build::cargo_cfg_windows() && (build::cargo_feature("windows_raw_dylib") || try_registry()) {
         return Ok(());
     }
 
     system_deps::Config::new().probe()?;
     Ok(())
+}
+
+#[cfg(not(windows))]
+fn try_registry() -> bool {
+    false
 }
 
 #[cfg(windows)]
@@ -20,34 +22,42 @@ fn try_registry() -> bool {
 
     use winreg::{enums::*, RegKey};
 
-    fn try_key(path: &str) -> bool {
+    fn try_key(path: &str, wide: bool) -> bool {
+        let flags = if wide {
+            KEY_WOW64_64KEY
+        } else {
+            KEY_WOW64_32KEY
+        };
         let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-        let Ok(key) = hklm
-            .open_subkey_with_flags(path, KEY_WOW64_32KEY | KEY_READ)
-            .inspect_err(|e| eprintln!("unable to retrieve install location: {e}"))
-        else {
+        let Ok(key) = hklm.open_subkey_with_flags(path, KEY_READ | flags) else {
             return false;
         };
         let Ok(root) = key
             .get_value::<OsString, _>("Install Directory")
             .map(PathBuf::from)
-            .inspect_err(|e| eprintln!("unable to retrieve install location: {e}"))
         else {
             return false;
         };
 
-        println!("detected install via registry: {}", root.display());
-        build::rustc_link_search(root.join("lib"));
-        build::rustc_link_lib("dylib:+verbatim=libgpg-error.imp");
-        true
-    }
-
-    if !build::cargo_cfg_windows() {
-        eprintln!("cross compiling. disabling registry detection.");
-        return false;
+        match (build::cargo_cfg_pointer_width(), wide) {
+            (64, true) | (32, false) => {
+                println!("detected install via registry: {}", root.display());
+                build::rustc_link_search(root.join("lib"));
+                build::rustc_link_lib("dylib:+verbatim=libgpg-error.imp");
+                true
+            }
+            _ => {
+                eprintln!(
+                    "An incompatible installation of GnuPG was detected: {}\n\
+                     Try switching the target from 64-bit to 32-bit or 32-bit to 64-bit.\n",
+                    root.display()
+                );
+                false
+            }
+        }
     }
 
     [r"SOFTWARE\Gpg4win", r"SOFTWARE\GnuPG"]
         .iter()
-        .any(|s| try_key(s))
+        .any(|s| try_key(s, true) || try_key(s, false))
 }
